b72189fe03891d1f4c01e84140ad29e673f7b5aa
Ravindra reddy Somula <reddysomula6789@gmail.com>
2024-07-22 22:54:10 +0530

    changes

--- config/upload.js
+++ config/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // Set storage engine
 const storage = multer.diskStorage({
-  destination: './uploads/',
+  destination: './uploads/apartments',
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   },
